ac78f4c44cc02cac7c681df399180fcb23e2f674
interface11 <kiranimani@gmail.com>
2017-01-17 17:05:21 -0600

    Update script.js

--- newside/js/script.js
+++ newside/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
                   $("button").click(function(){
- $("p").fadeIn(5000);fadeOut(1000);
-$("img").fadeIn(3000);fadeOut(1000);
+ $("p").fadeIn(5000);fadeOut(5000);
+$("img").fadeIn(3000);fadeOut(5000);
                   });
 });
